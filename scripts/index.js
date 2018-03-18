7fa8c39867b9b2c88746aa77382697e67bc6a36e
esdes-tonnaire-dimitri <32100364+esdes-tonnaire-dimitri@users.noreply.github.com>
2018-03-18 15:22:30 +0100

    Improve JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,39 @@
 var functionBasedDelay = anime({
-    targets: 'article',
-    translateX: 250,
-    direction: 'alternate',
-    loop: true,
-    delay: function(el, i, l) {
-      return 500+ i * 100;
-    }
+  targets: 'article',
+  translateX: 250,
+  direction: 'alternate',
+  loop: true,
+  delay: function (el, i, l) {
+    return 500 + i * 100;
+  }
+});
+
+var city = [
+  "Monaco",
+  "Lyon",
+  "Paris",
+  "Nice",
+  "Nantes",
+  "Lille",
+  "Besançon",
+  "Dijon",
+  "Annecy",
+]
+
+function getValue() {
+  var movie = document.getElementById('city').value;
+  var answer = document.getElementById('answer');
+  console.log(city);
+  if (movies.indexOf(city) !== -1) {
+    answer.textContent = city + " : Il est génial";
+  } else {
+    answer.textContent = city + " : Je ne connais pas ce film";
+  }
+
+  anime({
+    targets: answer,
+    translateY: -100,
+    opacity: 1,
+    direction: 'alternate'
   });
+}
